5a8319f40606ec1d08b89a6e8565defdc3ccc1a3
Anjali2210 <anjali.dube27@gmail.com>
2018-09-18 16:49:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './landing';
+import Search from './search';
+
+const FourohFour = () => <h1>404</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <div className='app'>
+        <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/search" component={Search} />
+            <Route component={FourohFour} />
+        </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+
+render(<App/>, document.getElementById('app'));
